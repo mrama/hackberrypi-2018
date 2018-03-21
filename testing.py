730f210f0cfd85d801fddf9e56f8d7cb132a0019
Rama <rmannava@andrew.cmu.edu>
2018-03-20 20:05:33 -0500

    basic skeleton

--- testing.py
+++ testing.py
@@ -3,6 +3,6 @@ import picamera
 with picamera.PiCamera() as camera:
 	camera.start_preview()
 	sleep(1)
-	camera.capture('/home/pi/Desktop/hackberrypi-2018/image1.jpg')
+	camera.capture('/home/pi/hackberrypi-2018/image1.jpg')
 	camera.stop_preview
 	
