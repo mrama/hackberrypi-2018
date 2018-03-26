3da3e8f3c021e9e32e6a30c1f2c96d1992466d96
Hana Frluckaj <hfrlucka@andrew.cmu.edu>
2018-03-26 00:33:38 -0400

    camera stuff

--- bin/camera.py
+++ bin/camera.py
@@ -1,8 +1,14 @@
 from time import sleep
 import picamera
+
+def range(start, end, step):
+	while start <= end:
+		yield start
+		start += step
+
 with picamera.PiCamera() as camera:
-	camera.start_preview()
-	sleep(1)
-	camera.capture('/home/pi/hackberrypi-2018/image1.jpg')
-	camera.stop_preview
+	camera.start_recording('video.mp4')
+	for x in range(1, 60, 0.5):
+		sleep(5)
+	camera.stop_recording()
 	
