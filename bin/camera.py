f9675a57693dee4b005a7f70762eb7b2c734d99a
Hana Frluckaj <hfrlucka@andrew.cmu.edu>
2018-03-26 01:14:42 -0400

    changing counter

--- bin/camera.py
+++ bin/camera.py
@@ -1,7 +1,7 @@
 from time import sleep
 import picamera
 
-destination = '/bin/videos/vids'
+destination = '/bin/videos'
 
 def get_file_name():
 	reutnr datetime.datetime.now().strftime("%m-%d_%H.%M.%S.mp4")
@@ -14,7 +14,7 @@ def range(start, end, step):
 with picamera.PiCamera() as camera:
 	camera.start_preview()
 	camera.start_recording("%s%s" % destination, get_file_name())
-	for x in range(1, 60, 0.5):
+	for x in range(1, 120, 0.5):
 		sleep(5)
 	camera.stop_recording()
 	camera.stop_preview()
