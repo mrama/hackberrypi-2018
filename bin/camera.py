0a2e7657803771b7ad1f963379d37581db387738
Hana Frluckaj <hfrlucka@andrew.cmu.edu>
2018-03-26 00:57:40 -0400

    specific file name for videos

--- bin/camera.py
+++ bin/camera.py
@@ -1,6 +1,11 @@
 from time import sleep
 import picamera
 
+destination = '/bin/videos/vids'
+
+def get_file_name():
+	reutnr datetime.datetime.now().strftime("%m-%d_%H.%M.%S.mp4")
+
 def range(start, end, step):
 	while start <= end:
 		yield start
@@ -8,7 +13,7 @@ def range(start, end, step):
 
 with picamera.PiCamera() as camera:
 	camera.start_preview()
-	camera.start_recording('video.mp4')
+	camera.start_recording("%s%s" % destination, get_file_name())
 	for x in range(1, 60, 0.5):
 		sleep(5)
 	camera.stop_recording()
