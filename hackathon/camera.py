0fa679a6efd1d89f721881f66e393c1c5092afc7
Hana Frluckaj <hfrlucka@andrew.cmu.edu>
2018-03-26 23:22:29 -0400

    photos should overwrite old ones

--- hackathon/camera.py
+++ hackathon/camera.py
@@ -4,7 +4,9 @@ from time import sleep
 camera = picamera.PiCamera()
 
 camera.start_preview()
-for i in range(0, 120, 1):
-    sleep(1);
-    camera.capture("/home/pi/hackberrypi-2018/hackathon/pictures/image-" + str(i) + ".jpg");
+while True:
+	for i in range(5):
+    		sleep(1);
+    		camera.capture("/home/pi/hackberrypi-2018/hackathon/pictures/image-" + str(i) + ".jpg");
+
 camera.stop_preview()
