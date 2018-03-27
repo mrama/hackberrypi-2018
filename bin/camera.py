84556fab57e8560a960eda871ac8f75f188aaa35
Rama <rmannava@andrew.cmu.edu>
2018-03-26 22:29:05 -0500

    get rid of annoying pictures in repo

--- bin/camera.py
+++ bin/camera.py
@@ -3,19 +3,19 @@ import picamera
 
 destination = '/bin/videos'
 
-def get_file_name():
-	reutnr datetime.datetime.now().strftime("%m-%d_%H.%M.%S.mp4")
+def get_file_name(i):
+    return ("video-" + str(i) + ".mp4")
 
 def range(start, end, step):
-	while start <= end:
-		yield start
-		start += step
+    while start <= end:
+        yield start
+        start += step
 
 with picamera.PiCamera() as camera:
-	camera.start_preview()
-	camera.start_recording("%s%s" % destination, get_file_name())
-	for x in range(1, 120, 0.5):
-		sleep(x)
-	camera.stop_recording()
-	camera.stop_preview()
-	
+    camera.start_preview()
+    while True:
+        for x in range(1, 120, 0.5):
+            camera.start_recording("%s%s" % destination, get_file_name(x))
+            sleep(x)
+            camera.stop_recording()
+    camera.stop_preview()
