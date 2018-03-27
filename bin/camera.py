75321606c05ae41955451b14eb212434db71e23e
Hana Frluckaj <hfrlucka@andrew.cmu.edu>
2018-03-26 23:44:13 -0400

    starting to implement snippets of video

--- bin/camera.py
+++ bin/camera.py
@@ -1,21 +1,11 @@
 from time import sleep
 import picamera
 
-destination = '/bin/videos'
-
-def get_file_name(i):
-    return ("video-" + str(i) + ".mp4")
-
-def range(start, end, step):
-    while start <= end:
-        yield start
-        start += step
-
 with picamera.PiCamera() as camera:
     camera.start_preview()
     while True:
-        for x in range(1, 120, 0.5):
-            camera.start_recording("%s%s" % destination, get_file_name(x))
-            sleep(x)
+        for x in range(30):
+            camera.start_recording("/home/pi/hackberrypi-2018/bin/vids/vid-" + str(i) + ".mp4");
+            sleep(5);
             camera.stop_recording()
     camera.stop_preview()
